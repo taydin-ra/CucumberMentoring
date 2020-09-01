7b67acc7afd5512bb8c9b7d18f1366a0676f14a0
taydin-ra <ttorun16@gmail.com>
2020-09-01 18:53:01 -0400

    rest assured mentoring notes are added

---
+++ src/test/java/restAssured/BasqarRestAssured.java
@@ -0,0 +1,93 @@
+package restAssured;
+
+import cucumber.api.java.ca.I;
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookies;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class BasqarRestAssured {
+            /*
+            Checklist
+
+            -correct url
+            -authentication
+            - correct request (get, put,post,delete)
+            -if body is necessary or not
+            -correct response
+
+        POJO- Plain Old Java Object
+            swagger documentation for api
+             */
+
+
+    @Test
+    public void creatingUser() {
+        Map<String, String> loginCredentials = new HashMap<>();
+        loginCredentials.put("username", "daulet2030@gmail.com");
+        loginCredentials.put("password", "TechnoStudy123@");
+
+        Cookies cookies = given()
+                .contentType(ContentType.JSON)
+                .body(loginCredentials)
+                .when()
+                .post("https://test.basqar.techno.study/auth/login")
+                .then()
+                .statusCode(200)
+                .extract().response().getDetailedCookies();
+        ;
+        ItemCategory item = new ItemCategory();
+        List<String> roleList = new ArrayList<>();
+        roleList.add("5b8bedceb1e0bfc07b008829");
+        item.setActive(true);
+        item.setName("TechnoIte213");
+        item.setSchoolId("5c5aa8551ad17423a4f6ef1d");
+        item.setRoles(roleList);
+
+        String itemId = given()
+                .cookies(cookies)
+                .contentType(ContentType.JSON)
+                .body(item)
+                .when()
+                .post("https://test.basqar.techno.study/school-service/api/item-category")
+                .then()
+                .log().body()
+                .statusCode(201)
+                .extract().jsonPath().getString("id");
+
+        // search our item
+
+        Map<String, String> searchItem = new HashMap<>();
+        searchItem.put("name", item.getName());
+        searchItem.put("schoolId", item.getSchoolId());
+
+        given()
+                .cookies(cookies)
+                .contentType(ContentType.JSON)
+                .body(searchItem)
+                .when()
+                .post("https://test.basqar.techno.study/school-service/api/item-category/search")
+                .then()
+                .log().body()
+                .statusCode(200);
+
+        // Delete the item
+
+        given()
+                .cookies(cookies)
+                .contentType(ContentType.JSON)
+                .when()
+                .delete("https://test.basqar.techno.study/school-service/api/item-category/" + itemId)
+                .then()
+                .statusCode(204);
+
+    }
+}
---
+++ src/test/java/restAssured/ItemCategory.java
@@ -0,0 +1,47 @@
+package restAssured;
+
+import java.util.List;
+
+public class ItemCategory {
+
+
+    private String name;
+    private Boolean active;
+    private String schoolId;
+    private List<String> roles;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public String getSchoolId() {
+        return schoolId;
+    }
+
+    public void setSchoolId(String schoolId) {
+        this.schoolId = schoolId;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+
+
+
+}
